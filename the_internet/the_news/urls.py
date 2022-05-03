09f0614c3911f03e24929b29dd2541f8c4d536b6
Jeadie <jackeadie@duck.com>
2022-05-04 08:41:43 +1000

    create urls for per month and week

--- the_internet/the_news/urls.py
+++ the_internet/the_news/urls.py
@@ -1,5 +1,7 @@
+from datetime import datetime
+
 from django.conf import settings
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework import routers
 
 from .views import InternetLocationRestViewSet, InternetNewsRestViewSet, InternetNewsIndexView
@@ -11,8 +13,13 @@ else:
     
 router.register("news", InternetNewsRestViewSet)
 router.register("locations", InternetLocationRestViewSet)
+months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun","Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+
 
 urlpatterns = [
+    re_path('^(?P<week>[0-9]{1,2})', InternetNewsIndexView.as_view(), name='news_week'),
+    re_path(f'^(?P<month>({"|".join([x.lower() for x in months])}))/$', InternetNewsIndexView.as_view(), name='news_month'),
+    re_path(f'^(?P<month>({"|".join([x.lower() for x in months])}))/' + '(?P<week>[0-9]{1,2})/$', InternetNewsIndexView.as_view(), name='news_month_week'),
     path('', InternetNewsIndexView.as_view(), name='news_page'),
     path("", include(router.urls)),
 ]
