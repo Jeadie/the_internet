6cc799cf693899ed2761b194b0666a1c8f7fe362
Jeadie <jackeadie@duck.com>
2022-05-06 09:22:12 +1000

    add local commands for running application, and logging dir

---
+++ tear_down.sh
@@ -0,0 +1,6 @@
+
+
+kill -9 $(ps | grep " python manage.py runserver" | cut -f1 -d " " | head -n 1)
+kill -9 $(ps | grep celery | grep worker | cut -f1 -d " ") # Multiple worker processes possible
+kill -9 $(ps | grep celery | grep beat | cut -f1 -d " " | head -n 1)
+kill -9 $(ps | grep "/usr/local/sbin/rabbitmq-server" | cut -f1 -d " " | head -n 1)
