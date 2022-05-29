c5dbb3693648b8d04c2e6204eb5a227be918fc52
jeadi <jeadi@amazon.com>
2022-05-29 10:49:10 +1000

    add whitenoise and remove static hosting on nginx

--- the_internet/the_internet/local_settings.py
+++ the_internet/the_internet/local_settings.py
@@ -3,6 +3,7 @@ from the_internet.settings import BASE_DIR
 SECRET_KEY = 'django-insecure-h*(16ugbn%yv_$@02=%r_@-s1kcfbyzxdx)7=9okcyb9p#+7p-'
 DEBUG=True
 
+CSRF_TRUSTED_ORIGINS = ["http://0.0.0.0:1337"]
 ALLOWED_HOSTS = ["localhost", "0.0.0.0", "127.0.0.1"]
 
 DATABASES = {
--- the_internet/the_internet/settings.py
+++ the_internet/the_internet/settings.py
@@ -52,6 +52,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'the_internet.middleware.HealthCheckMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -124,7 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
+STATICFILES_DIRS = [BASE_DIR / 'static',]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
