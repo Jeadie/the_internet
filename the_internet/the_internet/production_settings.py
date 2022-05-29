02a201ff5841ec600474696cd3727dce8a379c09
Jeadie <jackeadie@duck.com>
2022-05-29 10:49:10 +1000

    add whitenoise and remove static hosting on nginx

--- the_internet/the_internet/production_settings.py
+++ the_internet/the_internet/production_settings.py
@@ -15,8 +15,8 @@ import os
 
 DEBUG=False
 SECRET_KEY = os.environ["DJANGO_SECRET_KEY"]
-ALLOWED_HOSTS = ["the-internet-server.eba-tasfmrba.us-east-1.elasticbeanstalk.com", "news.onceaday.fyi", "onceaday.fyi"]
-# ALLOWED_HOSTS = ["*"]
+# ALLOWED_HOSTS = ["the-internet-server.eba-tasfmrba.us-east-1.elasticbeanstalk.com", "news.onceaday.fyi", "onceaday.fyi"]
+ALLOWED_HOSTS = ["*"]
 
 DATABASES = {
     'default': {
