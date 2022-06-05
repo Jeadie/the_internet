402d3999d2ca54b1c8a5ef49bfa7bc9ee5a2eb9b
Jeadie <jackeadie@duck.com>
2022-06-05 20:45:47 +1000

    add templated react app ts frontend

---
+++ frontend/once-a-day/src/model.ts
@@ -0,0 +1,12 @@
+
+export interface InternetContent {
+	url: string;
+	timestamp: number;
+	location: string;
+	title: string;
+	description: string;
+	mainCategory: string;
+	upvotes: number;
+	comments: number;
+	imageSourceURL: string;
+} 
