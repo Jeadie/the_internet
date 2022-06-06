8cb6ee3f17d39791f560480cca74de1c0bcb3c75
jeadi <jeadi@amazon.com>
2022-06-07 07:48:32 +1000

    add api.onceaday.link as custom domain for REST server

--- infrastructure/apigateway.tf
+++ infrastructure/apigateway.tf
@@ -46,3 +46,36 @@ resource "aws_lambda_permission" "api_gw" {
 
   source_arn = "${aws_apigatewayv2_api.backend.execution_arn}/*/*"
 }
+
+
+// TODO: Get this certificate in terraform
+resource "aws_apigatewayv2_domain_name" "api_domain" {
+  domain_name     = "api.onceaday.link"
+
+  domain_name_configuration {
+    certificate_arn = "arn:aws:acm:us-east-1:383495223751:certificate/af929853-2e80-41c5-ad81-ca2dd8bd367a"
+    endpoint_type = "REGIONAL"
+    security_policy = "TLS_1_2"
+  }
+
+}
+
+// TODO: get this hosted zone id in terraform
+resource "aws_route53_record" "api_domain_record" {
+  name    = aws_apigatewayv2_domain_name.api_domain.domain_name
+  type    = "A"
+  zone_id  = "Z102289216LD1O6ETDCYH"
+
+  alias {
+    name                   = aws_apigatewayv2_domain_name.api_domain.domain_name_configuration[0].target_domain_name
+    zone_id                = aws_apigatewayv2_domain_name.api_domain.domain_name_configuration[0].hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+
+resource "aws_apigatewayv2_api_mapping" "api_domain_to_gateway" {
+  api_id      = aws_apigatewayv2_api.backend.id
+  stage  = aws_apigatewayv2_stage.default_backend.id
+  domain_name = aws_apigatewayv2_domain_name.api_domain.domain_name
+}
