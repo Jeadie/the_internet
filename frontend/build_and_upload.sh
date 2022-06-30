676400a5fa94032728383cf898c31d8ee0fc2ab1
Jeadie <jackeadie@duck.com>
2022-06-30 14:58:05 +1000

    add --force flag to create CDN invalidation

--- frontend/build_and_upload.sh
+++ frontend/build_and_upload.sh
@@ -1,5 +1,24 @@
 
+
+
+echo $1
+exit 1
+
 cd once-a-day/
+
+# Make sure we don't deploy a local build.
+if ! grep -q "const isLocal = false" src/index.tsx; then
+    echo "isLocal=true... change to isLocal=false in index.tsx"
+    exit 1
+fi 
+
 npm run build
 cd -
 ./upload_build.sh
+
+
+# Create invalidation on `--force`
+if [$1 == "--force"]; then
+    echo "[INFO] - Creating a AWS CDN validation" 
+    aws cloudfront create-invalidation --distribution-id $(aws cloudfront list-distributions --query 'DistributionList.Items[0].Id' | sed 's/"//g') --paths /
+fi
